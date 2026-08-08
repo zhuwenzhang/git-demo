7858f621144e4c60f3338dd18df44d22fa762340
zhuwenzhang <1755137656@qq.com>
2026-08-08 20:26:03 +0800

    20260808-02

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ import torch
 print(torch.__version__)              # 应显示版本号
 print(torch.cuda.is_available())      # 应返回 True（GPU 可用）
 print(torch.cuda.get_device_name(0))  # 应显示 "NVIDIA GeForce RTX 4070"
+
